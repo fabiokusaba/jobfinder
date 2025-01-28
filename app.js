855bbd57d5992aaf43ccfafe2c06f706c3cbe03f
Fabio Kusaba <fabinhokb44@gmail.com>
2025-01-27 21:22:53 -0300

    conectando com o banco de dados e realizando testes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import db from './db/connection.js'
 
 const app = express()
 const PORT = 3000
@@ -7,6 +8,17 @@ app.listen(PORT, () => {
   console.log(`O Express está rodando na porta ${PORT}`)
 })
 
+// DB Connection
+db
+  .authenticate()
+  .then(() => {
+    console.log('Conectou ao banco com sucesso')
+  })
+  .catch(err => {
+    console.log('Ocorreu um erro ao conectar ', err)
+  })
+
+// Routes
 app.get('/', (req, res) => {
   res.send('Está funcionando 3')
 })
