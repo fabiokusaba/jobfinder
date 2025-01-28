93dfc1ac01d6f7e9f4d5f9248e73cc9eb239834d
Fabio Kusaba <fabinhokb44@gmail.com>
2025-01-27 21:01:46 -0300

    iniciando o app com express

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+const PORT = 3000
+
+app.listen(PORT, () => {
+  console.log(`O Express está rodando na porta ${PORT}`)
+})
+
+app.get('/', (req, res) => {
+  res.send('Está funcionando 3')
+})
