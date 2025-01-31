1ca78a07e067a397b6f19a948784d932e98de615
Fabio Kusaba <fabinhokb44@gmail.com>
2025-01-31 20:01:56 -0300

    criação do modelo Job

---
+++ models/Job.js
@@ -0,0 +1,23 @@
+import { Sequelize } from "sequelize";
+import db from "../db/connection.js";
+
+export const Job = db.define('job', {
+  title: {
+    type: Sequelize.STRING,
+  },
+  description: {
+    type: Sequelize.STRING,
+  },
+  salary: {
+    type: Sequelize.STRING,
+  },
+  company: {
+    type: Sequelize.STRING,
+  },
+  email: {
+    type: Sequelize.STRING,
+  },
+  new_job: {
+    type: Sequelize.INTEGER,
+  }
+})
