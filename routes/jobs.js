c30a42d82abfcb8b3e4edcf0eb222e53546d5bee
Fabio Kusaba <fabinhokb44@gmail.com>
2025-01-31 20:20:23 -0300

    criação das rotas de jobs

---
+++ routes/jobs.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import Job from '../models/Job.js'
+
+const router = express.Router()
+
+router.get('/test', (req, res) => {
+  res.send('deu certo')
+})
+
+// POST - adicionar job
+router.post('/add', (req, res) => {
+  const { title, salary, company, description, email, new_job } = req.body
+
+  // Insert - inserindo dados no sistema
+  Job.create({
+    title,
+    description,
+    salary,
+    company,
+    email,
+    new_job
+  }).then(() => {
+    res.redirect('/')
+  }).catch(err => console.log(err))
+})
+
+export default router
