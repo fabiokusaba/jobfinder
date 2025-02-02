413529fec087d9f6e098e7a22102d34cce10f98a
Fabio Kusaba <fabinhokb44@gmail.com>
2025-02-02 09:47:46 -0300

    adicionado handlebars ao projeto

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import exphbs from 'express-handlebars'
+import path from 'path'
 import db from './db/connection.js'
 import bodyParser from 'body-parser'
 import jobsRoutes from './routes/jobs.js'
@@ -12,6 +14,11 @@ app.listen(PORT, () => {
 
 // body parser
 app.use(bodyParser.urlencoded({ extended: false }))
+
+// handle bars
+app.set('views', path.join(__dirname, 'views'))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 // DB Connection
 db
